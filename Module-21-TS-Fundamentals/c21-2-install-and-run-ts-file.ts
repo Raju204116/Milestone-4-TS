136581e3f42c7483e3ec781b8e47634e78458cd5
Raju204116 <raju204116@gmail.com>
2026-08-17 18:17:10 +0600

    c21-2

---
+++ Module-21-TS-Fundamentals/c21-2-install-and-run-ts-file.ts
@@ -0,0 +1,76 @@
+
+/* 
+
+✅✅✅Install TypeScript & Run a .ts File
+✅1. Check Node.js
+node -v
+npm -v
+
+If both work, you're ready.
+
+✅2. Create a project
+
+mkdir typescript-practice (or,create manually)
+cd typescript-practice
+
+Initialize npm:
+npm init -y
+
+✅3. Install TypeScript
+
+➡️Option A — Install locally (recommended)
+npm install -D typescript
+
+➡️Option B — Install globally
+npm install -g typescript
+
+
+➡️Check:
+npx tsc --version
+or, tsc -v
+
+
+
+✅5. Create a TypeScript file
+
+Create:
+app.ts
+
+let name: string = "Raju";
+let age: number = 25;
+console.log(name);
+console.log(age);
+
+
+✅6. Compile TypeScript
+npx tsc
+TypeScript will compile .ts files into .js files according to your tsconfig.json.
+
+For a single file, you can also use:
+npx tsc app.ts
+
+This generates:
+app.js
+
+
+✅7. Run the JavaScript file
+node app.js
+
+Output:
+Raju
+25
+
+
+➡️Complete workflow
+app.ts
+  ↓
+npx tsc
+  ↓
+app.js
+  ↓
+node app.js
+
+Remember: Node.js runs the generated JavaScript; tsc compiles/checks the TypeScript.
+
+
+*/
