63a89340922f2032542366c2d4dcb4d85b20427e
Raju204116 <raju204116@gmail.com>
2026-08-17 20:25:13 +0600

    c21-10

---
+++ Module-21-TS-Fundamentals/c21-10-practice-task.ts
@@ -0,0 +1,128 @@
+/* 
+
+Module 21 — Problem Set
+
+Problem -1:
+একটি function বানাও যা একটি user-এর নাম আর বয়স নিয়ে sentence print করবে।
+Input:
+printUser("Amina", 22)
+Output:
+Amina is 22 years old.Problem Statement:
+
+Problem -2:
+একটি product-এর নাম, price, এবং availability store করো primitive types use করে।
+Input:
+name = "Laptop"
+price = 45000
+isAvailable = true
+Output:
+Laptop cost 45000 and is available: true
+
+
+Problem -3:
+একটি array বানাও যেখানে student-এর নাম থাকবে এবং কতজন আছে সেটা কনসোল করবে।
+Input:
+["Amina", "Rahim", "Karim", "Salma", "Rafi"]
+Output:
+Total student: 5
+
+
+Problem -5:
+একটি tuple বানাও যেখানে একজন মানুষের নাম আর বয়স থাকবে।
+Input:
+["Amina", 22]
+Output:
+Amina is 22 years old.
+
+
+Problem -6:
+একটি user object বানাও যেখানে name required, কিন্তু email optional।
+Input:
+{ name: "Amina" }
+Output:
+Name: Amina, Email: Not provided
+
+
+Problem -7:
+একটি function লেখো যা একটি number নিয়ে check করবে সেটা even কি না।
+Input:
+isEven(8)
+Output:
+true
+
+
+Problem -8:
+একটি function বানাও যা যেকোনো সংখ্যক number নিয়ে তাদের sum করবে।
+Input:
+sumAll(1, 2, 3, 4)
+Output:
+10
+
+
+Problem -9:
+দুইটি array merge করো spread operator ব্যবহার করে।
+Input:
+[1, 2, 3]
+[4, 5, 6]
+Output:
+[1, 2, 3, 4, 5, 6]
+
+
+Problem -10:
+একটি number positive না negative তা ternary operator দিয়ে বের করো।
+Input:
+num = 5
+Output:
+Positive
+
+
+Problem -11:
+একটি variable undefined হলে default value print করো।
+Input:
+username = undefined
+Output:
+Guest
+
+
+Problem -12:
+একটি unknown type variable declare করো এবং পরে type check করে string হলে print করো।
+Input:
+"Hello"
+Output:
+Hello
+
+
+Problem -13:
+একটি TypeScript program বানাও যেখানে একজন user-এর নাম, বয়স, email, skills, আর active status থাকবে। এরপর সব তথ্য print করবে।
+Input:
+{
+  name: "Amina",
+  age: 22,
+  email: "amina@email.com",
+  skills: ["HTML", "CSS", "TypeScript"],
+  active: true
+}
+Output:
+Name: Amina
+Age: 22
+Email: amina@email.com
+Skills: HTML, CSS, TypeScript
+Active: true
+
+
+
+
+
+Problem -14:Extra Challenge Ideas
+
+একটি shopping cart object বানাও।
+একটি function দিয়ে user login check করো।
+array থেকে even numbers filter করো।
+nested object থেকে country বের করো।
+type-safe function দিয়ে total price calculate করো।
+
+
+
+
+
+*/
