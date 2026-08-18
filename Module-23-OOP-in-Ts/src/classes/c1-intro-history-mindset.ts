346a8be90bc608e951fcc174af49072d779afc44
Raju204116 <raju204116@gmail.com>
2026-08-18 20:03:33 +0600

    c22-1

---
+++ Module-23-OOP-in-Ts/src/classes/c1-intro-history-mindset.ts
@@ -0,0 +1,45 @@
+
+/* 
+✅✅History of Simula-1960-Norway
+Simulation of ships in a port. (fuel, )
+
+
+
+✅✅What is OOP ?
+//We previously learned/programming in procedural way / functional way
+//OOP-Object Oriented Programing is a way of programming
+
+✅✅OOP has 4 pillars -i.e- oop was born to solve four problems
+
+1.Models the real world
+2.Hides complexity
+3.Reuses code
+4.Scales with teams
+
+
+✅✅Where oop needs -
+1. Big complex systems
+2.team collaboration
+3.code that changes often
+4.Simualtions and games
+
+
+✅✅where oop don't need
+ - in simple project
+ -normal function
+ -----
+
+
+✅✅initial commands to create and run ts file
+To create tsconfig : npx tsc--init
+To convert ts to js : tsc 
+To run file : node ./dist/classes/index.js
+
+To run without converting/tsc => install : npm i -g tsx
+then only type everytime :  tsx ./src/classes/index.ts
+
+
+*/
+
+const name:string ="Hello World";
+console.log(name);
