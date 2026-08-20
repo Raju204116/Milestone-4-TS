60f927065561959113e8f8cb3cfaa2e478e60a5a
Raju204116 <raju204116@gmail.com>
2026-08-20 20:11:32 +0600

    c24-10

--- Module-24-Problem-solving/src/c24-10-bank-transaction-processor.ts
+++ Module-24-Problem-solving/src/c24-10-bank-transaction-processor.ts
@@ -2,8 +2,10 @@
 ✅Problem 9 — Bank Transaction Processor
 Concepts: union types, discriminated unions, type narrowing, object types, return types, conditional logic
 Function name must be: processTransaction
+
 Scenario
 A banking application needs to process deposits and withdrawals. Every transaction contains a type and an amount.
+
 Deposit
 {
     type: "deposit",
@@ -14,14 +16,24 @@ Withdrawal
     type: "withdraw",
     amount: 1500
 }
+
 Task
-Create a function named processTransaction. It should receive the current account balance and a transaction, and return the new balance.
+Create a function named processTransaction. It should receive the current account balance and a transaction, 
+and return the new balance.
+
 Rules
-A deposit increases the balance.
-A withdrawal decreases the balance.
-A customer cannot withdraw more money than they currently have — in that case, the original balance should remain unchanged.
+    A deposit increases the balance.
+    A withdrawal decreases the balance.
+    A customer cannot withdraw more money than they currently have — in that case, the original balance should
+    remain unchanged.
+
+
 TypeScript Requirement
-Represent the two possible transaction shapes using TypeScript's type system. The function should not accept arbitrary transaction types.
+Represent the two possible transaction shapes using TypeScript's type system. The function should not accept 
+arbitrary transaction types.
+
+
+
 Starter Code
 // TODO: define types for Deposit and Withdrawal transactions
  
@@ -44,3 +56,25 @@ processTransaction(5000, { type: "withdraw", amount: 7000 });
 
 
 */
+
+type Transaction = {
+    type: "deposit" | "withdraw",
+    amount: number
+}
+
+function processTransaction(currentBalance: number, transaction:Transaction ):number {
+
+    if (transaction.type === "deposit") {
+        return currentBalance + transaction.amount;
+    } else if (transaction.type === "withdraw") {
+        if (transaction.amount > currentBalance  ) {
+            return currentBalance;
+        } 
+    }   
+    return currentBalance - transaction.amount;
+    
+
+}
+console.log(processTransaction(5000, { type: "deposit", amount: 2000 }));// 7000
+console.log(processTransaction(5000, { type: "withdraw", amount: 2000 }));// 3000
+console.log(processTransaction(5000, { type: "withdraw", amount: 7000 }));// 5000  (unchanged)  // insufficient balance
