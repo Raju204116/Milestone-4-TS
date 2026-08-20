6b35397f696be5b110028710b4c19d3775023bff
Raju204116 <raju204116@gmail.com>
2026-08-20 15:46:17 +0600

    c24-4

--- Module-24-Problem-solving/src/c24-4-social-media-profile-formatter.ts
+++ Module-24-Problem-solving/src/c24-4-social-media-profile-formatter.ts
@@ -3,14 +3,21 @@
 ✅Problem 3 — Social Media Profile Formatter
 Concepts: object types, type/interface, function parameter typing, return types, template literals
 Function name must be: formatUserProfile
+
 Scenario
-A social media application stores basic information about its users. The application needs a function that converts a user's structured data into a human-readable profile summary. A user contains name, age, and city.
+A social media application stores basic information about its users. The application needs a function that converts
+a user's structured data into a human-readable profile summary. A user contains name, age, and city.
+
 Task
-First define an appropriate TypeScript type or interface for the user. Then create a function named formatUserProfile. The function should receive a user object and return a formatted sentence.
+First define an appropriate TypeScript type or interface for the user. Then create a function named formatUserProfile.
+The function should receive a user object and return a formatted sentence.
+
 Requirements
-Accept a properly typed user object.
-Return a string.
-Use the values from the object rather than hard-coding the result.
+    Accept a properly typed user object.
+    Return a string.
+    Use the values from the object rather than hard-coding the result.
+
+
 
 Starter Code
 // TODO: define a type or interface for the user
@@ -21,7 +28,6 @@ function formatUserProfile(user: User): <type> {
  
 }
 
-
 Example
 formatUserProfile({
     name: "Fahim",
@@ -31,4 +37,32 @@ formatUserProfile({
  
 // Expected output:
 // "Fahim is 22 years old and lives in Dhaka."
+
 */
+
+
+
+type User={
+    name:string
+    age:number
+    city:string
+}
+ 
+// interface User{
+//     name:string
+//     age:number
+//     city:string
+// }
+
+function formatUserProfile(user: User): string {
+ 
+    return `${user.name} is ${user.age} years old and lives in ${user.city}.`
+ 
+}
+
+
+console.log(formatUserProfile({name: "Fahim",age: 22,city: "Dhaka"}));
+// "Fahim is 22 years old and lives in Dhaka."
+
+console.log(formatUserProfile({name: "Rafi",age: 20,city: "Gazipur"}));
+//Rafi is 20 years old and lives in Gazipur.
