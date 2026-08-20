c2b23dc90339076fa9d95d615aec0adf50fd9d8c
Raju204116 <raju204116@gmail.com>
2026-08-20 15:22:49 +0600

    c24-2

--- Module-24-Problem-solving/src/c24-2-cinema-ticket-counter.ts
+++ Module-24-Problem-solving/src/c24-2-cinema-ticket-counter.ts
@@ -2,7 +2,6 @@
 
 ✅✅✅TypeScript Basic Problem-Solving 
 
-
 ✅General Rules
 Do not change the function names. Use the exact name given in each problem.
 Every problem must be solved using a function.
@@ -20,29 +19,30 @@ Concepts: function parameter types, return types, number, conditional logic
 Function name must be: getTicketPrice
 
 Scenario
-A cinema is building a ticketing system. The ticket price depends on the customer's age because children and senior citizens receive special pricing. You are responsible for creating the function that determines how much a customer should pay for a single ticket.
+A cinema is building a ticketing system. The ticket price depends on the customer's age because children and 
+senior citizens receive special pricing. You are responsible for creating the function that determines 
+how much a customer should pay for a single ticket.
 
 Task
-Create a function named getTicketPrice. The function should receive the customer's age and return the appropriate ticket price.
+Create a function named getTicketPrice. The function should receive the customer's age and 
+return the appropriate ticket price.
 
-Pricing Rules
-Age
-Ticket Price
-Below 5
-0
-5–12
-100
-13–59
-200
-60 or above
-120
+Pricing Rules:
+
+Age                     Ticket Price
+Below 5                      0
+5–12                        100
+13–59                       200
+60 or above                 120
 
 A child younger than 5 years old can enter for free.
 
+
 Function Requirement
-Accept the age as a number.
-Return the ticket price as a number.
-Correctly handle all age ranges.
+    Accept the age as a number.
+    Return the ticket price as a number.
+    Correctly handle all age ranges.
+
 
 Starter Code
 function getTicketPrice(age:<type>): <type> {
@@ -51,19 +51,35 @@ function getTicketPrice(age:<type>): <type> {
  
 }
 Example
-getTicketPrice(3);
-// 0
- 
-getTicketPrice(10);
-// 100
- 
-getTicketPrice(25);
-// 200
- 
-getTicketPrice(65);
-// 120
+getTicketPrice(3);// 0
+getTicketPrice(10);// 100
+getTicketPrice(25);// 200
+getTicketPrice(65);// 120
 */
 
+
+
+function getTicketPrice(age:number):number  {
+ 
+    let price:number=0;
+    if(age<5){
+        price=0;
+    }else if(age<13){
+        price=100;
+    }else if (age<60){
+        price =200;
+    }else{
+        price=120;
+    }
+        
+    
+    return price;
+ 
+}
+console.log(getTicketPrice(3));// 0
+console.log(getTicketPrice(10));// 100
+console.log(getTicketPrice(25));// 200
+console.log(getTicketPrice(65));// 120
 
 
 
